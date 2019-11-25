8a7425b7666cd3f34c346e7330770055c4d59fcb
heryanq <heryan_@hotmail.com>
2019-11-25 07:06:05 -0300

    Test-driven development Pilha(household battery) example.

---
+++ tddPilha/Exceptions/PilhaCheiaException.java
@@ -0,0 +1,8 @@
+package Exceptions;
+
+public class PilhaCheiaException extends RuntimeException {
+
+    public PilhaCheiaException(String message) {
+        super(message);
+    }
+}
---
+++ tddPilha/Exceptions/PilhaVaziaException.java
@@ -0,0 +1,8 @@
+package Exceptions;
+
+public class PilhaVaziaException extends RuntimeException {
+
+    public PilhaVaziaException(String message) {
+        super(message);
+    }
+}
